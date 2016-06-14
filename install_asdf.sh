0019a5a00939d0e187436f238c886fdc43621e05
Óscar de Arriba <odarriba@gmail.com>
2016-06-14 10:29:21 +0200

    Update install_asdf.sh

--- install_asdf.sh
+++ install_asdf.sh
@@ -70,6 +70,7 @@ installSoftware() {
 
 	asdf plugin-add ruby https://github.com/asdf-vm/asdf-ruby.git;
 	asdf plugin-add node https://github.com/asdf-vm/asdf-nodejs.git;
+	asdf plugin-add erlang https://github.com/asdf-vm/asdf-erlang.git;
 	asdf plugin-add elixir https://github.com/asdf-vm/asdf-elixir.git;
 
 }
